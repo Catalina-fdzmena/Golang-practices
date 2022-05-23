487142169dab29d874b31a8bc83e0a46858dd99a
Catalina-fdzmena <A01197705@itesm.mx>
2022-05-23 11:24:01 -0500

    new practices

---
+++ practice2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var valor = 3.1416
+	valor2 := 3.1416789543
+	var valor3 float64 = 3.14267895
+
+	var cadena = "Hola, Mundo"
+	cadena2 := "Hola, Mundo"
+	var cadena3 string = "Hola, Mundo"
+
+	fmt.Println(valor, valor2, valor3)
+	fmt.Println(cadena, cadena2, cadena3)
+
+	//**********************************************
+
+}
