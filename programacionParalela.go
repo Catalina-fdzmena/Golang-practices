6cc2c422b9fc845848c45f45eb2719a68a3bcd43
Catalina-fdzmena <A01197705@itesm.mx>
2022-05-30 11:59:30 -0500

    changezzzzzz

--- programacionParalela.go
+++ programacionParalela.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func hola_hilo(id int) {
+func hola_hilo(id int, c chan int) {
 	fmt.Printf("Hola, soy Panchito %d concurrente\n", id)
+	c <- id
 }
 
 func main() {
+	//Canles para comunicación de hilos
+
+	c := make(chan int)
+
 	for i := 0; i < 20; i++ {
-		go hola_hilo(i)
+		go hola_hilo(i, c)
+
 	}
 
-	time.Sleep(30 * time.Second) //Con este comando se desarrolla comando para desarrolle un intervalo de la reproducción
+	for i := 0; i < 20; i++ {
+		resultado := <-c
+		if resultado == i {
+			fmt.Println("Un hilo ha acabado", resultado)
+		}
+	}
+	//time.Sleep(30 * time.Second) //Con este comando se desarrolla comando para desarrolle un intervalo de la reproducción en bajo nivel
 }
 
 //Inicia hilo procesamiento pero no lo marca de regreso por el tiempo que toma un cambio de contexto.
